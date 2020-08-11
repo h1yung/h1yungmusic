6bdfabb007430a0bbdeb17ef865b2063edcc4c9f
Hyeong Kyun (Daniel) Park <h1yungpark@gmail.com>
2020-08-11 04:26:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'Collection by Daniel.'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
