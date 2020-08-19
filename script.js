7fb3a1eef487996026107cb9740fe796590fd06f
Hyeong Kyun (Daniel) Park <park1119@purdue.edu>
2020-08-19 14:40:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Collection by Daniel.'; /* The text */
+var txt = 'Collection by Daniel'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
